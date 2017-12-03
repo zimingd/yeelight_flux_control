e18eef019ad7b5a7daf561d8cf303d6139075338
Ziming Dong <zimingd@gmail.com>
2017-12-03 05:42:02 -0800

    intentation and spacing

--- yeelight_flux_control.py
+++ yeelight_flux_control.py
@@ -14,7 +14,7 @@ FLUX_LISTEN_PORT = 42069
 
 class FluxColorTempHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-    	# respond to f.lux so it doesn't keep trying to resend
+        # respond to f.lux so it doesn't keep trying to resend
         self.send_response(200)
         self.end_headers()
 
@@ -24,7 +24,6 @@ class FluxColorTempHTTPRequestHandler(BaseHTTPRequestHandler):
         color_temp = int(params['ct'])
         brightness = int(round(float(params['bri']) * 100))  # flux's brightness levels are a bit low, maybe adjust?
 
-
         print(f"changing brightness to: {brightness}")
         brightness_change_msg = f'{{"id": 1, "method": "set_bright", "params":[{brightness},"{TRANSITION_TYPE}",{TRANSITION_DURATION_MS}]}}\r\n'
 
@@ -32,7 +31,7 @@ class FluxColorTempHTTPRequestHandler(BaseHTTPRequestHandler):
         color_temp_change_msg = f'{{"id": 1, "method": "set_ct_abx", "params":[{color_temp},"{TRANSITION_TYPE}",{TRANSITION_DURATION_MS}]}}\r\n'
 
         for ip in YEELIGHT_IPS:
-        	#no reason to keep socket alive since updates should be infrequent
+            #no reason to keep socket alive since updates should be infrequent
             with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
                 s.connect((ip, YEELIGHT_CONTROL_PORT))
                 s.send(brightness_change_msg.encode())
@@ -42,7 +41,7 @@ class FluxColorTempHTTPRequestHandler(BaseHTTPRequestHandler):
 print(f"Configure f.lux to post to: http://localhost:{FLUX_LISTEN_PORT}")
 server = HTTPServer(('', FLUX_LISTEN_PORT), FluxColorTempHTTPRequestHandler)
 try:
-	server.serve_forever()
+    server.serve_forever()
 except KeyboardInterrupt:
-	pass
+    pass
 server.server_close()
